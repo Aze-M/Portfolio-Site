1b2cc0685193954003a949058c3fb53741a47b27
Aze-M <archmageazekil@gmail.com>
2022-11-14 20:32:15 +0100

    Typo fixing.

--- src/pages/code.tsx
+++ src/pages/code.tsx
@@ -21,9 +21,9 @@ export default class Code extends React.Component {
                     In addition to using the react-router-dom package, as well as vite to set up dev server and the baasic codebase, <br />
                     a few vanilla JS functions have been written (also in Typescript) to handle the DOM manipulation. <br />
                     <br />
-                    The Use of React to make this just made making a lot of transitions on the page easier, as the base HTML never changes this also lets the backgroudn animation simply keep flowing, and the background change smoothly. <br />
+                    The use of React to make this just made making a lot of transitions on the page easier, as the base HTML never changes this also lets the backgroudn animation simply keep flowing, and the background change smoothly. <br />
                     <br />
-                    The <span className="fancy-blue tgl-me">componentDidMount()</span> function handles all of the class adding that results in the css morphing the website from page to page. <br />
+                    The <span className="fancy-blue tgl-me">componentDidMount()</span> function handles all of the class adding that results in CSS morphing the website from page to page. <br />
                     <br />
                 </div>
                 <div className="center tgl-me">Why not try a few more background colors?</div>
