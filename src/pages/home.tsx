aa5a85af1dfcdbd3f97ad5bbf8903d474953dcac
Aze-M <archmageazekil@gmail.com>
2022-11-24 15:11:12 +0100

    Content update

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -21,14 +21,14 @@ export default class Home extends React.Component {
                     <span className="font-brush fancy-blue font-larger tgl-me">Hello</span>, I am Christian Dirks. <br />
                     As of 2022 I've begun switching from tech-support, to tech-create. <br />
                     <br />
-                    Having previous experience in Programming making plugins for older games in lua API's, I have now made two sites with <span className="fancy-blue tgl-me">React</span> and <span className="fancy-lora tgl-me">JS</span> <br />
+                    Having previous experience in Programming making plugins for older games in lua API's, I have now made a few sites with <span className="fancy-blue tgl-me">React</span> and <span className="fancy-lora tgl-me">JS</span> <br />
                     <br />
                     My main site can be found <a href="https://aze-m.com" id="ws-link" className="fancy-blue tgl-me" target="_blank">here</a>. <br /> 
                     It has a private codebase, as well as implementation of a few <span className="fancy-dora tgl-me">express.js</span> backends which are closed off to outside requests. <br />
                 </div>
                 <img src={pic} alt="My face" className="img inline tgl-me" id="pr_img" />
                 <div className="text-bt center tgl-me">
-                    Now there's also this site, which serves as a sort of Portfolio to show off what I can do. <br />
+                    Now additionally there is this site, which serves as one of the Portfolios to show off what I can do. <br />
                     <br /> 
                     The codebase for this can be found in the code tab and is fully public, allowing you to see both the <span className="fancy-blue tgl-me">CSS</span> and the <span className="fancy-lora tgl-me">JS (Typescripted)</span> behind it. <br /> 
                     <br /> 
